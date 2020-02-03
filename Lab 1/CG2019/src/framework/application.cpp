ead5e135ad9674653b5eb3aabe92706a4e2a6efc
Gerard Camps Romaguera <gerardcampsromaguera@MacBook-Pro-de-Gerard-3.local>
2020-02-03 12:56:43 +0100

    Initial commit

---
+++ Lab 1/CG2019/src/framework/application.cpp
@@ -0,0 +1,288 @@
+#include "application.h"
+#include "utils.h"
+#include "image.h"
+
+Application::Application(const char* caption, int width, int height)
+{
+	this->window = createWindow(caption, width, height);
+
+	// initialize attributes
+	// Warning: DO NOT CREATE STUFF HERE, USE THE INIT 
+	// things create here cannot access opengl
+	int w,h;
+	SDL_GetWindowSize(window,&w,&h);
+
+	this->window_width = w;
+	this->window_height = h;
+	this->keystate = SDL_GetKeyboardState(NULL);
+    this->render_mode = DEF_MODE;
+    this->defCol = Color::BLACK;
+
+	framebuffer.resize(w, h);
+}
+
+//Here we have already GL working, so we can create meshes and textures
+void Application::init(void)
+{
+	std::cout << "initiating app..." << std::endl;
+
+    for ( int i = 0; i < 100; i++)
+    {
+        
+        particles[i].x = random() % (int)window_width;
+        particles[i].y = random() % (int)window_height;
+        particles[i].size = random() % (int)5;
+        int randNum = random() % 255;
+        particles[i].color = Color(randNum, randNum, randNum);
+    }
+	//here add your init stuff
+    framebuffer.fill(Color::WHITE);
+}
+
+//render one frame
+void Application::render( Image& framebuffer )
+{
+    if ( keystate[SDL_SCANCODE_SPACE] )
+    {
+        framebuffer.fill(Color::BLACK);
+        for (unsigned int i = 0; i < 100; i++)
+        {
+            if (!framebuffer.snowAnimation((int)particles[i].x, (int)particles[i].y, particles[i].size, particles[i].color))
+            {
+                particles[i].x = random() % (int)window_width;
+                particles[i].y = window_height - 1;
+             }
+        }
+    }
+    if ( render_mode == PAINT_MODE )
+    {
+        if( mouse_state & SDL_BUTTON(SDL_BUTTON_LEFT))
+        {
+            if ( mouse_position.x < window_width && mouse_position.x > 0 && mouse_position.y < window_height && mouse_position.y > toolBar.height)
+            {
+                framebuffer.drawCircle((int)mouse_position.x, (int)mouse_position.y, 2, defCol, true);
+            }
+        }
+    }
+    else
+    {
+        if ( render_mode == LINE_MODE)
+            framebuffer.drawLine(random() % (int)window_width, random() % (int)(window_height - toolBar.height), random() % (int)window_width, random() % (int)window_height, defCol);
+        if ( render_mode == CIRCLE_MODE)
+            framebuffer.drawCircle(random() % (int)window_width, random() % (int)(window_height - toolBar.height), random() % (int)50, defCol, random() % 2 < 1);
+        if ( render_mode == RECTANGLE_MODE)
+            framebuffer.drawRectangle(random() % (int)window_width, random() % (int)(window_height - toolBar.height), random() % (int)50, random() % (int)50, defCol, random() % 2 < 1);
+        if ( render_mode == CENTROID_MODE)
+            framebuffer.formulaCentroid();
+        if ( render_mode == GRADIENT1_MODE)
+            framebuffer.gradientBlueRed();
+        if ( render_mode == GRADIENT2_MODE)
+            framebuffer.gradientRedGreenSteps();
+        if ( render_mode == GRAYSCALE_MODE)
+            framebuffer.grayScale();
+        if ( render_mode == TRESHOLD_MODE)
+            framebuffer.treshold();
+        if ( render_mode == RESIZE_MODE )
+            framebuffer.scale(2);
+        if ( render_mode == ROTATE_MODE )
+            framebuffer.rotate(45);
+        if ( render_mode == RESTART_MODE)
+            framebuffer.fill(Color::WHITE);
+        render_mode = DEF_MODE;
+    }
+        
+    
+}
+
+//called after render
+void Application::update(double seconds_elapsed)
+{
+	//to see all the keycodes: https://wiki.libsdl.org/SDL_Keycode
+	if (keystate[SDL_SCANCODE_SPACE]) //if key space is pressed
+	{
+		    for ( int i = 0; i < 100; i++)
+            {
+                int rnd = random() % 10;
+                particles[i].x += (int)particles[i].y % 20 < 10 ? seconds_elapsed * rnd : seconds_elapsed * rnd * -1;
+                particles[i].y -= seconds_elapsed*10;
+            }
+	}
+	//to read mouse position use mouse_position
+}
+
+//keyboard press event 
+void Application::onKeyDown( SDL_KeyboardEvent event )
+{
+	//to see all the keycodes: https://wiki.libsdl.org/SDL_Keycode
+
+	switch(event.keysym.scancode)
+	{
+		case SDL_SCANCODE_ESCAPE:
+			exit(0); 
+			break; //ESC key, kill the app
+        case SDL_SCANCODE_1:
+            render_mode = LINE_MODE;
+            break;
+        case SDL_SCANCODE_2:
+            render_mode = RECTANGLE_MODE;
+            break;
+        case SDL_SCANCODE_3:
+            render_mode = CIRCLE_MODE;
+            break;
+        case SDL_SCANCODE_4:
+            render_mode = CENTROID_MODE;
+            break;
+        case SDL_SCANCODE_5:
+            render_mode = GRADIENT1_MODE;
+            break;
+        case SDL_SCANCODE_6:
+            render_mode = GRADIENT2_MODE;
+            break;
+        case SDL_SCANCODE_7:
+            render_mode = GRAYSCALE_MODE;
+            break;
+        case SDL_SCANCODE_8:
+            render_mode = TRESHOLD_MODE;
+            break;
+        case SDL_SCANCODE_9:
+            render_mode = ROTATE_MODE;
+            break;
+        case SDL_SCANCODE_0:
+            render_mode = RESIZE_MODE;
+            break;
+        case SDL_SCANCODE_R:
+            render_mode = RESTART_MODE;
+            break;
+        case SDL_SCANCODE_P:
+            render_mode = PARTICLE_SNOW_MODE;
+            break;
+        case SDL_SCANCODE_L:
+            // load a placeholder image
+            if (this->framebuffer.loadTGA("../../res/forest.tga") == false)
+            {
+                std::cout << "Image not found" << std::endl;
+            }
+            break;
+        case SDL_SCANCODE_E:
+            // load a placeholder image
+            loadToolBar();
+            render_mode = PAINT_MODE;
+            break;
+	}
+}
+
+//keyboard key up event 
+void Application::onKeyUp(SDL_KeyboardEvent event)
+{
+	//...
+    //render_mode = DEF_MODE;
+}
+
+//mouse button event
+void Application::onMouseButtonDown( SDL_MouseButtonEvent event )
+{
+	if (event.button == SDL_BUTTON_LEFT) //left mouse pressed
+	{
+		//if you read mouse position from the event, careful, Y is reversed, use mouse_position instead
+        
+	}
+}
+
+void Application::onMouseButtonUp( SDL_MouseButtonEvent event )
+{
+	if (event.button == SDL_BUTTON_LEFT) //left mouse unpressed
+	{
+        int start = 11;
+        int save = 60;
+        // save button occupies 30px, others 25
+        int saveEnd = 90;
+        int colorStart = saveEnd + 25;
+        if ( mouse_position.y < this->toolBar.height )
+        {
+            if ( mouse_position.x > start && mouse_position.x < start + 25)
+            {
+                loadToolBar();
+            }
+            if ( mouse_position.x > saveEnd - 30 && mouse_position.x < saveEnd )
+            {
+                Image imageToSave;
+                imageToSave.resize(framebuffer.width, framebuffer.height);
+                imageToSave.fill(Color::WHITE);
+                for ( int x = 0; x < framebuffer.width; x++)
+                {
+                    for ( int y = toolBar.width; y < framebuffer.height; y++)
+                    {
+                        imageToSave.setPixel(x, y, framebuffer.getPixel(x, y));
+                    }
+                }
+                if (imageToSave.saveTGA("../../res/myFile.tga") == false)
+                {
+                    std::cout << "Error saving the image." << std::endl;
+                }
+            }
+            if ( mouse_position.x > colorStart && mouse_position.x < colorStart + 25 )
+            {
+                defCol = Color::BLACK;
+            }
+            colorStart = colorStart + 50;
+            if ( mouse_position.x > colorStart && mouse_position.x < colorStart + 25 )
+            {
+                defCol = Color::RED;
+            }
+            colorStart = colorStart + 50;
+            if ( mouse_position.x > colorStart && mouse_position.x < colorStart + 25 )
+            {
+                defCol = Color::GREEN;
+            }
+            colorStart = colorStart + 50;
+            if ( mouse_position.x > colorStart && mouse_position.x < colorStart + 25 )
+            {
+                defCol = Color::BLUE;
+            }
+            colorStart = colorStart + 50;
+            if ( mouse_position.x > colorStart && mouse_position.x < colorStart + 25 )
+            {
+                defCol = Color::YELLOW;
+            }
+            colorStart = colorStart + 50;
+            if ( mouse_position.x > colorStart && mouse_position.x < colorStart + 25 )
+            {
+                defCol = Color::PURPLE;
+            }
+            colorStart = colorStart + 50;
+            if ( mouse_position.x > colorStart && mouse_position.x < colorStart + 25 )
+            {
+                defCol = Color::CYAN;
+            }
+            colorStart = colorStart + 50;
+            if ( mouse_position.x > colorStart && mouse_position.x < colorStart + 25 )
+            {
+                defCol = Color::WHITE;
+            }
+                    
+        }
+	}
+}
+
+//when the app starts
+void Application::start()
+{
+	std::cout << "launching loop..." << std::endl;
+	launchLoop(this);
+}
+
+void Application::loadToolBar()
+{
+    this->framebuffer.fill(Color::WHITE);
+    if (this->toolBar.loadTGA("../../res/toolbar.tga") == false)
+    {
+        std::cout << "Image not found" << std::endl;
+    }
+    for ( unsigned int x = 0; x < this->toolBar.width; x++ )
+    {
+        for ( unsigned int y = 0; y < this->toolBar.height; y++ )
+        {
+            framebuffer.setPixel(x, y, this->toolBar.getPixelRef(x, y));
+        }
+    }
+}
---
+++ Lab 1/CG2019/src/framework/image.cpp
@@ -0,0 +1,462 @@
+#include "image.h"
+
+
+Image::Image() {
+	width = 0; height = 0;
+	pixels = NULL;
+}
+
+Image::Image(unsigned int width, unsigned int height)
+{
+	this->width = width;
+	this->height = height;
+	pixels = new Color[width*height];
+	memset(pixels, 0, width * height * sizeof(Color));
+}
+
+//copy constructor
+Image::Image(const Image& c) {
+	pixels = NULL;
+
+	width = c.width;
+	height = c.height;
+	if(c.pixels)
+	{
+		pixels = new Color[width*height];
+		memcpy(pixels, c.pixels, width*height*sizeof(Color));
+	}
+}
+
+//assign operator
+Image& Image::operator = (const Image& c)
+{
+	if(pixels) delete pixels;
+	pixels = NULL;
+
+	width = c.width;
+	height = c.height;
+	if(c.pixels)
+	{
+		pixels = new Color[width*height*sizeof(Color)];
+		memcpy(pixels, c.pixels, width*height*sizeof(Color));
+	}
+	return *this;
+}
+
+Image::~Image()
+{
+	if(pixels) 
+		delete pixels;
+}
+
+
+
+//change image size (the old one will remain in the top-left corner)
+void Image::resize(unsigned int width, unsigned int height)
+{
+	Color* new_pixels = new Color[width*height];
+	unsigned int min_width = this->width > width ? width : this->width;
+	unsigned int min_height = this->height > height ? height : this->height;
+
+	for(unsigned int x = 0; x < min_width; ++x)
+		for(unsigned int y = 0; y < min_height; ++y)
+			new_pixels[ y * width + x ] = getPixel(x,y);
+
+	delete pixels;
+	this->width = width;
+	this->height = height;
+	pixels = new_pixels;
+}
+
+//change image size and scale the content
+void Image::scale(unsigned int width, unsigned int height)
+{
+	Color* new_pixels = new Color[width*height];
+
+	for(unsigned int x = 0; x < width; ++x)
+		for(unsigned int y = 0; y < height; ++y)
+			new_pixels[ y * width + x ] = getPixel((unsigned int)(this->width * (x / (float)width)), (unsigned int)(this->height * (y / (float)height)) );
+
+	delete pixels;
+	this->width = width;
+	this->height = height;
+	pixels = new_pixels;
+}
+
+Image Image::getArea(unsigned int start_x, unsigned int start_y, unsigned int width, unsigned int height)
+{
+	Image result(width, height);
+	for(unsigned int x = 0; x < width; ++x)
+		for(unsigned int y = 0; y < height; ++y)
+		{
+			if( (x + start_x) < this->width && (y + start_y) < this->height) 
+				result.setPixel( x, y, getPixel(x + start_x,y + start_y) );
+		}
+	return result;
+}
+
+void Image::flipX()
+{
+	for(unsigned int x = 0; x < width * 0.5; ++x)
+		for(unsigned int y = 0; y < height; ++y)
+		{
+			Color temp = getPixel(width - x - 1, y);
+			setPixel( width - x - 1, y, getPixel(x,y));
+			setPixel( x, y, temp );
+		}
+}
+
+void Image::flipY()
+{
+	for(unsigned int x = 0; x < width; ++x)
+		for(unsigned int y = 0; y < height * 0.5; ++y)
+		{
+			Color temp = getPixel(x, height - y - 1);
+			setPixel( x, height - y - 1, getPixel(x,y) );
+			setPixel( x, y, temp );
+		}
+}
+
+
+//Loads an image from a TGA file
+bool Image::loadTGA(const char* filename)
+{
+	unsigned char TGAheader[12] = {0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	unsigned char TGAcompare[12];
+	unsigned char header[6];
+	unsigned int bytesPerPixel;
+	unsigned int imageSize;
+
+	FILE * file = fopen(filename, "rb");
+   	if ( file == NULL || fread(TGAcompare, 1, sizeof(TGAcompare), file) != sizeof(TGAcompare) ||
+		memcmp(TGAheader, TGAcompare, sizeof(TGAheader)) != 0 ||
+		fread(header, 1, sizeof(header), file) != sizeof(header))
+	{
+		std::cerr << "File not found: " << filename << std::endl;
+		if (file == NULL)
+			return NULL;
+		else
+		{
+			fclose(file);
+			return NULL;
+		}
+	}
+
+	TGAInfo* tgainfo = new TGAInfo;
+    
+	tgainfo->width = header[1] * 256 + header[0];
+	tgainfo->height = header[3] * 256 + header[2];
+    
+	if (tgainfo->width <= 0 || tgainfo->height <= 0 || (header[4] != 24 && header[4] != 32))
+	{
+		std::cerr << "TGA file seems to have errors or it is compressed, only uncompressed TGAs supported" << std::endl;
+		fclose(file);
+		delete tgainfo;
+		return NULL;
+	}
+    
+	tgainfo->bpp = header[4];
+	bytesPerPixel = tgainfo->bpp / 8;
+	imageSize = tgainfo->width * tgainfo->height * bytesPerPixel;
+    
+	tgainfo->data = new unsigned char[imageSize];
+    
+	if (tgainfo->data == NULL || fread(tgainfo->data, 1, imageSize, file) != imageSize)
+	{
+		if (tgainfo->data != NULL)
+			delete tgainfo->data;
+            
+		fclose(file);
+		delete tgainfo;
+		return false;
+	}
+
+	fclose(file);
+
+	//save info in image
+	if(pixels)
+		delete pixels;
+
+	width = tgainfo->width;
+	height = tgainfo->height;
+	pixels = new Color[width*height];
+
+	//convert to float all pixels
+	for(unsigned int y = 0; y < height; ++y)
+		for(unsigned int x = 0; x < width; ++x)
+		{
+			unsigned int pos = y * width * bytesPerPixel + x * bytesPerPixel;
+			this->setPixel(x , height - y - 1, Color( tgainfo->data[pos+2], tgainfo->data[pos+1], tgainfo->data[pos]) );
+		}
+
+	delete tgainfo->data;
+	delete tgainfo;
+
+	return true;
+}
+
+// Saves the image to a TGA file
+bool Image::saveTGA(const char* filename)
+{
+	unsigned char TGAheader[12] = {0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+	FILE *file = fopen(filename, "wb");
+	if ( file == NULL )
+	{
+		fclose(file);
+		return false;
+	}
+
+	unsigned short header_short[3];
+	header_short[0] = width;
+	header_short[1] = height;
+	unsigned char* header = (unsigned char*)header_short;
+	header[4] = 24;
+	header[5] = 0;
+
+	//tgainfo->width = header[1] * 256 + header[0];
+	//tgainfo->height = header[3] * 256 + header[2];
+
+	fwrite(TGAheader, 1, sizeof(TGAheader), file);
+	fwrite(header, 1, 6, file);
+
+	//convert pixels to unsigned char
+	unsigned char* bytes = new unsigned char[width*height*3];
+	for(unsigned int y = 0; y < height; ++y)
+		for(unsigned int x = 0; x < width; ++x)
+		{
+			Color c = pixels[(height-y-1)*width+x];
+			unsigned int pos = (y*width+x)*3;
+			bytes[pos+2] = c.r;
+			bytes[pos+1] = c.g;
+			bytes[pos] = c.b;
+		}
+
+	fwrite(bytes, 1, width*height*3, file);
+	fclose(file);
+	return true;
+}
+
+void Image::drawRectangle(int start_x, int start_y, int w, int h, Color color, bool fill)
+{
+    if ( fill ){
+        for (unsigned int x = start_x; x < start_x + w; x++)
+        {
+            for (unsigned int y = start_y; y < start_y + h; y++)
+            {
+                setPixel(x, y, color);
+            }
+        }
+    }
+    else
+    {
+        for (unsigned int x = start_x; x < start_x + w; x++)
+        {
+            setPixel(x, start_y, color);
+            setPixel(x, start_y + h - 1, color);
+        }
+        for (unsigned int y = start_y; y < start_y + h; y++)
+        {
+            setPixel(start_x, y, color);
+            setPixel(start_x + w - 1, y, color);
+        }
+    }
+}
+
+void Image::drawCircle( int xCenter, int yCenter, int r, Color color, bool fill)
+{
+    float r2, h;
+    int x1square, x2square, y1square, y2square;
+    r2 = r * r;
+    //h = distance(xCenter, yCenter, xCenter + r, yCenter + r);
+    x1square = xCenter - r;
+    x2square = xCenter + r;
+    y1square = yCenter - r;
+    y2square = yCenter + r;
+    for (unsigned int x = x1square; x < x2square; x++)
+    {
+        for(unsigned int y = y1square; y < y2square; y++)
+        {
+            if ( x < width && x > 0 && y < height && y > 0)
+            {
+                if( fill )
+                    distance(xCenter, yCenter, x, y) < r ? setPixel(x, y, color) : (void)1;
+                else
+                {
+                    // normalize the value for the equation, if it'sa near 1 then it'll mean its near the real border
+                    float circleEq = ((x - xCenter) * (x - xCenter) + (y - yCenter) * (y - yCenter))/r2;
+                    circleEq > .95 && circleEq < 1.05  ? setPixel(x, y, color) : (void)1;
+                }
+            }
+        }
+    }
+}
+
+void Image::drawLine( int x1, int y1, int x2, int y2, Color color )
+{
+    // if the first point is greater than the second in the x-axis, swap them
+    if( x1 > x2 )
+    {
+        std::swap(x1, x2);
+        std::swap(y1, y2);
+    }
+    const int deltaX = x2 - x1;
+    const int deltaY = y2 - y1;
+    const float m = deltaY / (float)deltaX;
+    const float b = y1 - m * x1;
+    int y = y1;
+    for(unsigned int x = x1; x < x2; x++)
+    {
+        y = round(m * x + b);
+        setPixel(x, y, color);
+    }
+}
+
+void Image::formulaCentroid()
+{
+    // calculate the center of the image
+    const int xCenter = width / 2;
+    const int yCenter = height / 2;
+    // max distance to normalize it's going to be the one from 0,0 to the center
+    const float maxDist = distance(0, 0, xCenter, yCenter);
+    for (unsigned int x = 0; x < width; x++)
+    {
+        for (unsigned int y = 0; y < height; y++)
+        {
+            float normDist = distance(x, y, xCenter, yCenter) / maxDist;
+            
+            setPixel(x, y, Color( 255 * normDist, 255 * normDist, 255 * normDist ));
+        }
+    }
+}
+
+void Image::gradientBlueRed()
+{
+    for (unsigned int x = 0; x < width; x++)
+    {
+        for (unsigned int y = 0; y < height; y++)
+        {
+            // more distance in the x axis means more blue, less distance, more red
+            float xDist = x / (float)width;
+            setPixel(x, y, Color(255 * xDist, 0, 255 * ( 1 - xDist)));
+        }
+    }
+}
+
+void Image::gradientRedGreenSteps()
+{
+    float red = 0.0;
+    float green = 0.0;
+    for (unsigned int x = 0; x < width; x++)
+    {
+        for (unsigned int y = 0; y < height; y++)
+        {
+            // more distance in the x axis means more blue, less distance, more red
+            float xDist = x / (float)width;
+            float yDist = y / (float)height;
+            if (x % 80 == 0)
+            {
+                red = xDist * 255;
+            }
+            if (y % 60 == 0)
+            {
+                green = yDist * 255;
+            }
+            setPixel(x, y, Color(red, green, 0));
+        }
+    }
+}
+
+void Image::grayScale()
+{
+    Color pixel;
+    float meanValue;
+    for (unsigned int x = 0; x < width; x++)
+    {
+        for (unsigned int y = 0; y < height; y++ )
+        {
+            pixel = getPixel(x, y);
+            meanValue = (pixel.r + pixel.g + pixel.b) / 3;
+            setPixel(x, y, Color(meanValue, meanValue, meanValue));
+        }
+    }
+}
+
+void Image::treshold()
+{
+    float colorVal;
+    grayScale();
+    for ( unsigned int x = 0; x < width; x++)
+    {
+        for ( unsigned y = 0; y < height; y++)
+        {
+            getPixel(x, y).b < 128 ? colorVal = 0 : colorVal = 255;
+            setPixel(x, y, Color(colorVal, colorVal, colorVal));
+        }
+    }
+}
+
+void Image::rotate( int angle )
+{
+    //int xNew, yNew;
+    Color* new_pixels = new Color[width*height];
+    float sin45 = sin( angle * 3.14159 / 180 );
+    float cos45 = cos( angle * 3.14159 / 180 );
+    int xCenter = (int)(.5 * (width - 1));
+    int yCenter = (int)(.5 * (height - 1));
+    
+    for ( unsigned int x = 0; x < width; x++)
+    {
+        for (unsigned int y = 0; y < height; y++)
+        {
+            int a = x - xCenter;
+            int b = y - yCenter;
+            int xNew = (int)(a * cos45 - b * sin45 + xCenter);
+            int yNew = (int)(a * sin45 + b * cos45 + yCenter);
+            if (xNew < this->width && yNew < height && xNew >=0 && yNew >= 0)
+                new_pixels[y * width + x] = getPixel(xNew, yNew);
+        }
+    }
+    delete pixels;
+    pixels = new_pixels;
+}
+
+void Image::scale( float proportion )
+{
+    Color* new_pixels = new Color[width*height];
+    for ( unsigned int x = 0; x < width; x++)
+    {
+        for ( unsigned int y = 0; y < height; y++)
+        {
+            int xNew = x / proportion;
+            int yNew = y / proportion;
+            if (xNew < this->width && yNew < height && xNew >=0 && yNew >= 0)
+                new_pixels[y * width + x] = getPixel(xNew, yNew);
+        }
+    }
+    delete pixels;
+    pixels = new_pixels;
+}
+
+bool Image::snowAnimation(int x, int y, int size, Color color)
+{
+    if( x > 0 && x < width && y > 0 && y < height)
+    {
+        drawCircle(x, y, size, color, true);
+        return true;
+    }
+    else return false;
+}
+
+
+#ifndef IGNORE_LAMBDAS
+
+//you can apply and algorithm for two images and store the result in the first one
+//forEachPixel( img, img2, [](Color a, Color b) { return a + b; } );
+template <typename F>
+void forEachPixel(Image& img, const Image& img2, F f) {
+	for(unsigned int pos = 0; pos < img.width * img.height; ++pos)
+		img.pixels[pos] = f( img.pixels[pos], img2.pixels[pos] );
+}
+
+#endif
