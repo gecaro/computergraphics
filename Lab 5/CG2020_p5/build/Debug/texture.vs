9dd7acfadc1c3187a257caab2ccd51ed745a3ceb
Gerard Camps Romaguera <gerardcampsromaguera@MacBook-Pro-de-Gerard-3.local>
2020-03-10 23:06:40 +0100

    Added Lab 5 initial files,2 first points done

---
+++ Lab 5/CG2020_p5/build/Debug/texture.vs
@@ -0,0 +1,21 @@
+//global variables from the CPU
+uniform mat4 model;
+uniform mat4 viewprojection;
+
+
+//vars to pass to the pixel shader
+varying vec2 v_coord;
+varying vec3 v_wPos;
+varying vec3 v_wNormal;
+
+void main()
+{	
+	v_wPos = (model * gl_Vertex).xyz;
+	v_wNormal = (model * vec4( gl_Normal.xyz, 0.0)).xyz;
+
+	//get the texture coordinates (per vertex) and pass them to the pixel shader
+	v_coord = gl_MultiTexCoord0.xy;
+
+	//project the vertex by the model view projection 
+	gl_Position = viewprojection * vec4(v_wPos,1.0); //output of the vertex shader
+}
---
+++ Lab 5/CG2020_p5/src/framework/application.cpp
@@ -0,0 +1,136 @@
+#include "application.h"
+#include "utils.h"
+#include "image.h"
+#include "camera.h"
+#include "mesh.h"
+#include "shader.h"
+#include "texture.h"
+#include "light.h"
+
+Camera* camera = NULL;
+Mesh* mesh = NULL;
+Matrix44 model_matrix;
+Shader* shader = NULL;
+Texture* texture = NULL;
+Light* light = NULL;
+
+Application::Application(const char* caption, int width, int height)
+{
+	this->window = createWindow(caption, width, height);
+
+	// initialize attributes
+	// Warning: DO NOT CREATE STUFF HERE, USE THE INIT 
+	// things create here cannot access opengl
+	int w,h;
+	SDL_GetWindowSize(window,&w,&h);
+
+	this->window_width = w;
+	this->window_height = h;
+	this->keystate = SDL_GetKeyboardState(NULL);
+}
+
+//Here we have already GL working, so we can create meshes and textures
+void Application::init(void)
+{
+	std::cout << "initiating app..." << std::endl;
+	
+	//here we create a global camera and set a position and projection properties
+	camera = new Camera();
+	camera->lookAt(Vector3(0,20,20),Vector3(0,10,0),Vector3(0,1,0));
+	camera->setPerspective(60,window_width / window_height,0.1,10000);
+
+	//then we load a mesh
+	mesh = new Mesh();
+	mesh->loadOBJ("lee.obj");
+
+	//load the texture
+	texture = new Texture();
+	if(!texture->load("lee_color_specular.tga"))
+	{
+		std::cout << "Texture not found" << std::endl;
+		exit(1);
+	}
+
+	//we load a shader
+	shader = Shader::Get("texture.vs","texture.ps");
+    light = new Light();
+	//load whatever you need here
+	//......
+}
+
+//render one frame
+void Application::render(void)
+{
+	// Clear the window and the depth buffer
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable( GL_DEPTH_TEST );
+
+	//Get the viewprojection
+	camera->aspect = window_width / window_height;
+	Matrix44 viewprojection = camera->getViewProjectionMatrix();
+	
+	//enable the shader
+	shader->enable();
+	shader->setMatrix44("model", model_matrix); //upload info to the shader
+	shader->setMatrix44("viewprojection", viewprojection); //upload info to the shader
+    shader->setVector3("lightpos", light->position);
+    shader->setVector3("eyepos", camera->eye);
+    shader->setVector3("ia", Vector3(.23, .23, .23));
+    shader->setVector3("id", light->diffuse_color);
+    shader->setVector3("is", light->specular_color);
+
+	shader->setTexture("color_texture", texture, 0 ); //set texture in slot 0
+
+	//render the data
+	mesh->render(GL_TRIANGLES);
+
+	//disable shader
+	shader->disable();
+
+	//swap between front buffer and back buffer
+	SDL_GL_SwapWindow(this->window);
+}
+
+//called after render
+void Application::update(double seconds_elapsed)
+{
+	if (keystate[SDL_SCANCODE_SPACE])
+	{
+		model_matrix.rotateLocal(seconds_elapsed,Vector3(0,1,0));
+	}
+}
+
+//keyboard press event 
+void Application::onKeyPressed( SDL_KeyboardEvent event )
+{
+	switch(event.keysym.sym)
+	{
+		case SDLK_r: Shader::ReloadAll(); break;
+		case SDLK_ESCAPE: exit(0); break; //ESC key, kill the app
+	}
+}
+
+//mouse button event
+void Application::onMouseButtonDown( SDL_MouseButtonEvent event )
+{
+	if (event.button == SDL_BUTTON_LEFT) //left mouse pressed
+	{
+        Vector2 local = Vector2(model_matrix.frontVector().x, model_matrix.frontVector().z) ;
+        model_matrix.rotateLocal(ComputeSignedAngle(local, mouse_delta),Vector3(0,1,0));
+	}
+}
+
+void Application::onMouseButtonUp( SDL_MouseButtonEvent event )
+{
+	if (event.button == SDL_BUTTON_LEFT) //left mouse unpressed
+	{
+
+	}
+}
+
+//when the app starts
+void Application::start()
+{
+	std::cout << "launching loop..." << std::endl;
+	launchLoop(this);
+}
